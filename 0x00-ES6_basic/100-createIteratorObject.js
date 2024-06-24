f829a0eee82d88e26c8261457f883a5e517933b7
LyndaLilly <lilianlynda22@gmail.com>
2024-06-24 21:33:06 +0000

    tasks

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,7 +1,9 @@
 export default function createIteratorObject(report) {
-  const outputs = [];
-  for (const x of Object.values(report.allEmployees)) {
-    result.push(...x);
-  }
-  return outputs;
+  return (function* _() {
+    for (const alldep of Object.values(report.allEmployees)) {
+      for (const employee of alldep) {
+        yield employee;
+      }
+    }
+  }());
 }
