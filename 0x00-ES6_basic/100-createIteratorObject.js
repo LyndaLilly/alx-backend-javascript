48f95c63cf40f7e9188197f41dcbd64783e4562c
LyndaLilly <lilianlynda22@gmail.com>
2024-06-24 20:58:08 +0000

    tasks

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,7 @@
+export default function createIteratorObject(report) {
+  const outputs = [];
+  for (const x of Object.values(report.allEmployees)) {
+    result.push(...x);
+  }
+  return outputs;
+}
