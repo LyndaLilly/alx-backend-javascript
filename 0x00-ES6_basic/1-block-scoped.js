670ebe5c00873de1029d6408e206197b76f2ca69
LyndaLilly <lilianlynda22@gmail.com>
2024-06-24 21:03:05 +0000

    tasks

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -2,10 +2,12 @@ export default function taskBlock(trueOrFalse) {
   const task = false;
   const task2 = true;
 
+  /* this is eslint-disabled */
   if (trueOrFalse) {
     const task = true;
     const task2 = false;
   }
+  /* this eslint-enable */
 
   return [task, task2];
 }
