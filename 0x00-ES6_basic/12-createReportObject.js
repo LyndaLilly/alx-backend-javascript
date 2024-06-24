19a8517c3017958a669a0785db3c0600211b3393
LyndaLilly <lilianlynda22@gmail.com>
2024-06-24 21:24:35 +0000

    tasks

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,5 +1,10 @@
-export default function createEmployeesObject(departmentName, employees) {
+export default function createReportObject(employeesList) {
+  // get obj with all employee
   return {
-    [`${departmentName}`]: employees,
+    allEmployees: employeesList,
+    getNumberOfDepartments(employeesList) {
+      // gets bo of key in obj
+      return Object.keys(employeesList).length;
+    },
   };
 }
