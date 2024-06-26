cdafa5cc7ca02075fd08479024933ceeb3140eb6
LyndaLilly <lilianlynda22@gmail.com>
2024-06-26 19:13:03 +0000

    task 0

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,8 @@
+export default function getResponseFromAPI() {
+  return new Promise(() => {
+    // this returns new Promise(function(resolve, reject)
+    // this resolves data or anyither thing
+    // for successful response the resolve can handle the data retr
+    // for unsuccessful the reject function is called
+  });
+}
